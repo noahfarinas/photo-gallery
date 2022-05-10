8d62875ff4a35d2398b5171ba89ef0c6d9eff4b6
noahfarinas <94990436+noahfarinas@users.noreply.github.com>
2022-05-10 12:42:30 -0700

    changed css for navbar to fix display, center, sizes, and margins. more responsive now

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,50 +1,50 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
   return (
-    <nav className="nav-bar">
-      <h1>Noah Nigel </h1>
-      <ul
-        className={isMobile ? "nav-links-mobile" : "nav-links"}
-        onClick={() => setIsMobile(false)}
-      >
-        <Link to="/photo-gallery" className="home">
-          <li>Home</li>
-        </Link>
+    
+      <nav className="nav-bar">
+        <h1>Noah Nigel </h1>
+        <div
+          className={isMobile ? "nav-links-mobile" : "nav-links"}
+          onClick={() => setIsMobile(false)}
+        >
+          <Link to="/photo-gallery" className="home">
+            Home
+          </Link>
 
-        <Link to="/about" className="about">
-          <li>About</li>
-        </Link>
+          <Link to="/about" className="about">
+            About
+          </Link>
 
-        <Link to="/Gallery" className="gallery">
-          <li>Gallery</li>
-        </Link>
+          <Link to="/Gallery" className="gallery">
+            Gallery
+          </Link>
 
-        <Link to="/investment" className="investment">
-          <li>Investment</li>
-        </Link>
+          <Link to="/investment" className="investment">
+            Investment
+          </Link>
 
-        <Link to="/projects" className="projects">
-          <li>Code</li>
-        </Link>
+          <Link to="/projects" className="projects">
+            Code
+          </Link>
 
-        {/* <Link to="/contact" className="contact">
+          {/* <Link to="/contact" className="contact">
             <li>Contact</li>
           </Link> */}
-      </ul>
-      <button className="mobile-menu"
-        onClick={() => setIsMobile(!isMobile)}
-      >
-        {isMobile ? (
-          <ion-icon name="close-outline"></ion-icon>
-        ) : (
-          <ion-icon name="menu-outline"></ion-icon>
-        )}
-      </button>
-    </nav>
+        </div>
+        <button className="mobile-menu" onClick={() => setIsMobile(!isMobile)}>
+          {isMobile ? (
+            <ion-icon name="close-outline"></ion-icon>
+          ) : (
+            <ion-icon name="menu-outline"></ion-icon>
+          )}
+        </button>
+      </nav>
+   
   );
 };
 
